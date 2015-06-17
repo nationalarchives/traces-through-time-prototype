6b96490db00d29dcf8cd12d928265771953781ca
GTVJones <m@gwnjns.com>
2015-06-17 18:15:26 +0100

    Addition of first details page

--- includes/index-page-content.php
+++ includes/index-page-content.php
@@ -1,10 +1,13 @@
 <section class="row">
     <div class="col starts-at-full clr holding-box">
         <div class="heading-holding-banner">
-            <h2><span><span>[Page title]</span></span></h2>
+            <h2><span><span>Traces Through Time - user interface prototype</span></span></h2>
         </div>
         <div class="breather">
-            <p>[Page content]</p>
+            <p>Use the links below to navigate between prototypes:</p>
+            <ul>
+                <li>Discovery details page showing <a href="../details-page.php">related records</a></li>
+            </ul>
         </div>
     </div>
 </section>
