9e0e89f086838adfd24ccc06f2197b8e9d058cea
GTVJones <m@gwnjns.com>
2015-06-17 19:00:09 +0100

    Minor change - removal of unnecessary full stop.

--- includes/related-records-widget.php
+++ includes/related-records-widget.php
@@ -1,4 +1,4 @@
-<?php $rr = new TracesThroughTime\RelatedRecord('Name Lincoln, L V. Date of Birth: 31 May 1898', 95); ?>
+<?php $rr = new TracesThroughTime\RelatedRecord('Name Lincoln, L V Date of Birth: 31 May 1898', 95); ?>
 
 <div class="col starts-at-full ends-at-one-third clr holding-box">
     <h2><span><span>Related records</span></span></h2>
